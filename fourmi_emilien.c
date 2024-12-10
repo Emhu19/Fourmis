8f0f1aecfb38ee776611b8bc11fabf82ef507a58
Emhu19 <emilienhuret@gmail.com>
2024-12-10 13:25:13 +0100

    fourmieliere visuellement parfaite

--- fourmi_emilien.c
+++ fourmi_emilien.c
@@ -2,121 +2,134 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <unistd.h>
 
-#define MAX_CHAR_SALLE_NOM 50
-#define MAX_CHAR_ROLE 50
-#define MAX_CHAR_MALADIE 50
+#define MAX_CHAR 50
+#define MAX_NIVEAUX 6
 
 typedef struct {
-    char espece[MAX_CHAR_SALLE_NOM];
+    char espece[MAX_CHAR];
     int id_reine;
-    char role[MAX_CHAR_ROLE];
+    char role[MAX_CHAR];
     int spermatec;
     int age;
     float cgt;
     int faim;
     int eau;
     int sante;
-    char maladie[MAX_CHAR_MALADIE];
+    char maladie[MAX_CHAR];
 } Reine;
 
 typedef struct {
-    char espece[MAX_CHAR_SALLE_NOM];
+    char espece[MAX_CHAR];
     int id_fourmi;
     bool sexe;
-    char role[MAX_CHAR_ROLE];
+    char role[MAX_CHAR];
     int age;
     int faim;
     int eau;
     bool sante;
-    char maladie[MAX_CHAR_MALADIE];
+    char maladie[MAX_CHAR];
     float cgt;
     int coord_x;
     int coord_y;
 } Fourmi;
-
-typedef struct {
-    float temperature_moyenne;
-    float variation_temperature;
-    float temperature;
-    int precipitation;
-} Meteo;
-
-typedef struct {
-    int id_predateur;
-    char nom[MAX_CHAR_SALLE_NOM];
-    int coord_x;
-    int coord_y;
-    int sante;
-    int vitesse;
-    int force;
-} Predateur;
-
-typedef struct {
-    char biome[MAX_CHAR_SALLE_NOM];
-} Environnement;
-
-typedef struct {
-    int saison;
-    int mois;
-    int jour;
-} Temps;
-
-typedef struct {
-    char type[MAX_CHAR_SALLE_NOM];
-    int nourriture;
-    int materiaux;
-    int eau;
-} Chunk;
 
 void clear_terminal() {
     printf("\033[H\033[J");
 }
 
 void afficher_titre(const char *titre) {
-    printf("\n===== %s =====\n", titre);
+    printf("\n\033[1;34m===== %s =====\033[0m\n\n", titre);
 }
 
-void afficher_salle(const char *nom, const char *connexions) {
-    printf("    +-----------------+\n");
-    printf("    |                 |\n");
-    printf("    |     %3s         |\n", nom);
-    printf("    |                 |\n");
-    printf("    +-----------------+\n");
-    if (connexions) {
-        printf("        |%s\n", connexions);
-    }
+void afficher_salle_simple(const char *nom) {
+    printf("+-----------------+\n");
+    printf("|                 |\n");
+    printf("|     %-10s  |\n", nom);
+    printf("|                 |\n");
+    printf("+-----------------+\n");
 }
 
+void afficher_salle_simple_milieu(const char *nom) {
+   printf("                            +-----------------+\n");
+    printf("                            |                 |\n");
+    printf("                            |     %-10s  |\n", nom);
+    printf("                            |                 |\n");
+    printf("                            +-----------------+\n");
+}
+
+void afficher_connexion_simple() {
+    printf("        |\n");
+}
+
+void afficher_connexion_simple_milieu() {
+    printf("                                  |\n");
+}
+
+void afficher_connexion_double() {
+    printf("        |                            |\n");
+}
+
+void afficher_salles_alignees(const char *nom1, const char *nom2) {
+    printf("+-----------------+         +-----------------+\n");
+    printf("|                 |         |                 |\n");
+    printf("|     %-10s  |----+----|     %-10s  |\n", nom1, nom2);
+    printf("|                 |         |                 |\n");
+    printf("+-----------------+         +-----------------+\n");
+}
+
+void afficher_salles_alignees_3(const char *nom1, const char *nom2, const char *nom3) {
+    printf("+-----------------+         +-----------------+         +-----------------+\n");
+    printf("|                 |         |                 |         |                 |\n");
+    printf("|     %-10s  |----+----|     %-10s  |----+----|     %-10s  |\n", nom1, nom2, nom3);
+    printf("|                 |         |                 |         |                 |\n");
+    printf("+-----------------+         +-----------------+         +-----------------+\n");
+}
 void afficher_fourmiliere_niveau(int niveau) {
     switch (niveau) {
         case 1:
-            afficher_salle("A", NULL);
-            afficher_salle("B", NULL);
+            afficher_salles_alignees("R", "N");
             break;
         case 2:
-            afficher_salle("A", "|");
-            afficher_salle("C", NULL);
-            afficher_salle("B", NULL);
+            afficher_salles_alignees("R", "N");
+            afficher_connexion_simple();
+            afficher_salle_simple("L");
             break;
         case 3:
-            afficher_salle("A", "|");
-            afficher_salle("C", "|");
-            afficher_salle("B", NULL);
-            afficher_salle("D", NULL);
+            afficher_salles_alignees("R", "N");
+            afficher_connexion_double();
+            afficher_salles_alignees("L", "R");
+            break;
+        case 4:
+            afficher_salle_simple("R");
+            afficher_connexion_simple();
+            afficher_salles_alignees("L", "R");
+            break;
+        case 5:
+            afficher_salle_simple("R");
+            afficher_connexion_simple();
+            afficher_salles_alignees_3("L", "R", "R");
+            break;
+        case 6:
+            afficher_salle_simple("R");
+            afficher_connexion_simple();
+            afficher_salles_alignees_3("L", "R", "R");
+            afficher_connexion_simple_milieu();
+            afficher_salle_simple_milieu("L");
             break;
         default:
-            printf("Niveau non pris en charge.\n");
+            printf("Niveau %d non pris en charge.\n", niveau);
             break;
     }
 }
 
 void afficher_legende() {
-    printf("\nLégende :\n");
-    printf("A : Salle de la reine\n");
-    printf("B : Stockage de nourriture\n");
-    printf("C : Salle des larves\n");
-    printf("D : Stockage de ressources\n");
+    printf("\n\033[1;33mLégende :\033[0m\n");
+    printf("Reine : Salle de la reine\n");
+    printf("Nourriture : Stockage de nourriture\n");
+    printf("Larves : Salle des larves\n");
+    printf("Ressources : Stockage de ressources\n");
 }
 
 void afficher_fourmiliere(int niveau) {
@@ -124,12 +137,14 @@ void afficher_fourmiliere(int niveau) {
     afficher_titre("Fourmilière");
     afficher_fourmiliere_niveau(niveau);
     afficher_legende();
+    printf("\n\033[1;32mAppuyez sur Entrée pour continuer...\033[0m\n");
+    getchar();
 }
 
 int main() {
-    for (int niveau = 1; niveau <= 3; niveau++) {
+    for (int niveau = 1; niveau <= MAX_NIVEAUX; niveau++) {
         afficher_fourmiliere(niveau);
-        sleep(2);
     }
+
     return 0;
 }
